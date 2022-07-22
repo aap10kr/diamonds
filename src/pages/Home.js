ba20525a0fe453d4a9160377ef24af3a6ed77791
sineeeeeeee <aap10kr@naver.com>
2022-07-22 14:00:31 +0900

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,13 +25,48 @@ import sec8_ringImg from '../assets/images/sec8_ring.png'
 import footerImg from '../assets/images/fixed.png'
 import Hlogo_Img from '../assets/images/Hartlodo.png'
 
+import { useState, useEffect } from 'react'
+
 function Home() {
+
+  const [ScrollY, setScrollY] = useState(0);
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
+  
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if(ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
+    }
+  }
+
+  const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setScrollY(0);  // ScrollY 의 값을 초기화
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow)
+    }
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow)
+    }
+  })
+
   return (
     <div>
       {/* section1 start */}
       <div className="section_one pc_screen">
         <div className="container">
-
           <div className='sec1_main'>
             <div className='sec_1_left'>
               <div className='nav'>
@@ -529,6 +564,11 @@ function Home() {
         </div>
       </div>
       {/* section8 end */}
+      <div className='pc_screen'>
+        <button 
+          className={BtnStatus ? "topBtn active" : "topBtn"} // 버튼 노출 여부
+          onClick={handleTop}>TOP</button>
+      </div>
 
       {/* footer */}
       <footer>
